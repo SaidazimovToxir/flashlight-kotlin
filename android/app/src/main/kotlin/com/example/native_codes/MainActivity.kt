e52cec63b49de242dc95dbde26aa0f10ad9de896
tohir <st.coder2005@gmail.com>
2024-08-07 00:07:43 +0500

    some changes

--- android/app/src/main/kotlin/com/example/native_codes/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/native_codes/MainActivity.kt
@@ -1,5 +1,34 @@
 package com.example.native_codes
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import android.hardware.camera2.CameraManager
+import android.content.Context
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    private val CHANNEL = "com.example.flashlight/flashlight"
+    private var isFlashOn = false
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            call, result ->
+            if (call.method == "toggleFlashlight") {
+                isFlashOn = !isFlashOn
+                toggleFlashlight(isFlashOn)
+                result.success(isFlashOn)
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun toggleFlashlight(status: Boolean) {
+        val cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        val cameraId = cameraManager.cameraIdList[0]
+        cameraManager.setTorchMode(cameraId, status)
+    }
+}
